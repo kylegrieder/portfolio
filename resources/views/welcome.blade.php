63c40f69a02ab0dd92b6c56d8fb3bc03423583dd
Kyle <kyle.grieder@gmail.com>
2018-04-13 16:30:15 -0600

    minor changes, just getting started

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,32 +1,27 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>kyleg.net</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Comfortaa:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Bootstrap Core Styling -->
+        <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Bootstrap Custom Styling -->
+        <link href="css/cover.css" rel="stylesheet">
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+            <div class="inner-cover">
+                <div class="cover-heading">
+                    <h1>kyle grieder</h1>
                 </div>
 
                 <div class="links">
