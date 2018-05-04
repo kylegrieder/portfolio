c467d18daaa09c8df2f13c85947c70bd7fc84cf2
Kyle <kyle.grieder@gmail.com>
2018-05-04 10:59:43 -0600

    created photo and photos vue files

---
+++ resources/assets/js/components/Photos.vue
@@ -0,0 +1,22 @@
+<template>
+
+</template>
+
+<script>
+import store from '../store.js'
+import Photo from './Photo.vue'
+export default {
+	components: {
+		Photo
+	},
+
+	methods: {
+	},
+
+	data() {
+		return {
+			store
+		}
+	}, 
+}
+</script>
