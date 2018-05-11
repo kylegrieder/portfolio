64209e11962f722158bae2d8d484ef667181a46c
Kyle Grieder <kyle.grieder@gmail.com>
2018-05-10 22:33:02 -0600

    changed location of social media svg images to public/images/ and updated masthead so that images are accessable from any page

--- resources/views/nav/masthead.blade.php
+++ resources/views/nav/masthead.blade.php
@@ -13,9 +13,9 @@
                 <a href="/photos">Photos</a>
             </div>
             <div class="row-center">
-                <a href="https://twitter.com/kgfromthemv" target="_blank"><img class="svg" src="twitter.svg" alt="twitter"></img></a>
-                <a href="https://instagram.com/kgfromthemv" target="_blank"><img class="svg" src="instagram.svg" alt="instagram"></img></a>
-                <a href="https://github.com/kgfromthemv" target="_blank"><img class="svg" src="gitHub.svg" alt="gitHub"></img></a>
+                <a href="https://twitter.com/kgfromthemv" target="_blank"><img class="svg" src="/images/twitter.svg" alt="twitter"></img></a>
+                <a href="https://instagram.com/kgfromthemv" target="_blank"><img class="svg" src="/images/instagram.svg" alt="instagram"></img></a>
+                <a href="https://github.com/kgfromthemv" target="_blank"><img class="svg" src="/images/gitHub.svg" alt="gitHub"></img></a>
             </div>
         </div>
     </div>
